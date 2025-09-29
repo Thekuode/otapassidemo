64b322a87e609afe422f191299ad6193adf607c5
Roni <ronisvlnn@gmail.com>
2025-09-29 17:06:53 +0300

    raha txt ja korjauksia

--- src/app/komponentit/keskialue/keskialue.component.ts
+++ src/app/komponentit/keskialue/keskialue.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { RahaService } from '../../service/raha';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-keskialue',
@@ -7,5 +9,12 @@ import { Component } from '@angular/core';
   styleUrl: './keskialue.component.css'
 })
 export class KeskialueComponent {
+  raha: string | undefined;
 
+  constructor(private rahaService: RahaService) {}
+
+  async ngOnInit() {
+      this.raha = await firstValueFrom(this.rahaService.getRaha());
+      console.log(this.raha);
+  }
 }
