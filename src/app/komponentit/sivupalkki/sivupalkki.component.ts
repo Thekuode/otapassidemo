8af22e8e900c92925e916dcab9db6b509c3df98c
Roni <ronisvlnn@gmail.com>
2025-09-30 19:40:50 +0300

    kategoria valikoima

--- src/app/komponentit/sivupalkki/sivupalkki.component.ts
+++ src/app/komponentit/sivupalkki/sivupalkki.component.ts
@@ -1,17 +1,36 @@
 import { Component } from '@angular/core';
 import { RahaService } from '../../service/raha';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+
+enum RahaTapahtuma {
+  HUVIT = 'Huvit',
+  HYÖDYT = 'Hyödyt',
+  MUUT = 'Muut',
+  PAKOLLISEET = 'Pakolliset'
+}
 
 @Component({
   selector: 'app-sivupalkki',
-  imports: [],
+  imports: [NgSelectModule, FormsModule, CommonModule],
   templateUrl: './sivupalkki.component.html',
   styleUrl: './sivupalkki.component.css'
 })
 export class SivupalkkiComponent {
+  rahatapahtumat = Object.values(RahaTapahtuma);
+  valittu?: string;
+
   constructor(private rahaService: RahaService){}
 
   vahennarahaa(){
     this.rahaService.minusRaha();
     console.log(this.rahaService.getRaha());
+  }
+
+  valitseTapahtuma(event: Event) {
+  const value = (event.target as HTMLSelectElement).value;
+  this.valittu = value;
   }
 }
